753e51e5bce904e30b7a80725293a2f0b6c7fc0a
luowen <bigpao.luo@gmail.com>
2017-09-24 15:20:40 +0800

    addcustomer 404 page

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ for (let path in approutes) { //register all define router
 }
 app.use("/", router); // register router
 app.use("/static", express.static(path.join(__dirname, "public"))); // set web document root
+// set global error handle. must keep this at last route
+app.use((request, response, next) => {
+    response.status(404);
+    if (request.accepts("html")) {
+        return response.render("errors/404");
+    }
+    return response.send({ code: 1, msg: "ERROR", data: "404 NOT FOUND" });
+});
+
 app.set("views", "./public"); // set view file document root
 app.set("view engine", "ejs"); // set template engine
 app.listen(8888, () => {
